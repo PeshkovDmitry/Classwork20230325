4f5ba5742b3252ed1573027ffba91f02bcc456fc
Peshkov Dmitry <peshkov-dv@yandex.ru>
2023-03-25 15:22:31 +0300

    1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿void InputArray(int[] array) {
+    array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+    // for (int i = 0; i < array.Length; i++)
+    //     array[i] = new Random().Next(-9, 10); // [-9, 9] 
+} 
+
+bool ReleaseArray(int[] array) {
+    bool canBe = 
+        (array[0] < array[1] + array[2]) &&
+        (array[1] < array[0] + array[2]) &&
+        (array[2] < array[0] + array[1]);
+    return canBe;
+}
+
+Console.Clear(); 
+int[] array = new int[3]; 
+InputArray(array); 
+Console.WriteLine($"{ReleaseArray(array)}"); 
---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] InputArray() {
+    int [] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray(); 
+    return array;
+} 
+
+void ReleaseArray(int[] ar1, int[] ar2) {
+    for (int i = 0; i < ar1.Length; i++) {
+        ar2[i] = ar1[i];
+    }
+}
+
+Console.Clear(); 
+int[] a = InputArray();
+int[] b = new int[a.Length];  
+ReleaseArray(a, b);
+Console.WriteLine($"{String.Join(", ", b)}"); 
